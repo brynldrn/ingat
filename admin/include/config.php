81f780f8a855e9eb17825fdf596c6294bc0b85ad
OreoHolicookie <154807378+OreoHolicookie@users.noreply.github.com>
2025-03-04 08:09:49 +0800

    Update config.php

--- admin/include/config.php
+++ admin/include/config.php
@@ -8,7 +8,6 @@ $port = 3306;
 
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } else {
